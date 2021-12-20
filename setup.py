c345b2e79ad9db32b89fa78a71fa06e29717d8dc
Pablo M. Reyes <pablo.reyes@sri.com>
2021-12-20 01:07:23 -0800

    version 0.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ISRpy",
-    version="0.0.1",
+    version="0.0.4",
     author="Erhan Kudeki",
     author_email="erhan@illinois.edu",
     description="Radar Tools",
