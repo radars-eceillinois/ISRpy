4d73011ed01244c867b109825e39bd4fed71fb25
Pablo M. Reyes <pablo.reyes@sri.com>
2021-12-20 10:51:14 -0800

    advancing version to 0.0.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ISRpy",
-    version="0.0.6",
+    version="0.0.7",
     author="Erhan Kudeki",
     author_email="erhan@illinois.edu",
     description="Radar Tools",
